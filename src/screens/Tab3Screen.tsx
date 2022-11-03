5198798080dbb0cca230afb6fc995c54abaae3b2
Toni Cordon <toni.cordon@koobin.com>
2022-11-03 11:15:03 +0100

    add console log to screen

--- src/screens/Tab3Screen.tsx
+++ src/screens/Tab3Screen.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text } from 'react-native'
 
 export const Tab3Screen = () => {
+  useEffect(() => {
+    console.log('Tab3Screen')
+  }, [])
+
   return (
     <Text>Tab3Screen</Text>
   )
